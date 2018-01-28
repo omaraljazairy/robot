a9eb6f0a1d8606758973a61b70536310fa93d5f2
Omar <onha2001@fedal.com>
2018-01-28 22:05:22 +0100

    changed setup and GPIO

--- resources/direction.py
+++ resources/direction.py
@@ -11,6 +11,10 @@ gpio.setup(7, gpio.OUT)
 gpio.setup(11, gpio.OUT)
 gpio.setup(13, gpio.OUT)
 gpio.setup(15, gpio.OUT)
+gpio.output(7, False)
+gpio.output(11, False)
+gpio.output(13, False)
+gpio.output(15, False)
 
 logger = logging.getLogger('directions')
 
