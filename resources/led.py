4831f58078434cfc4dda8e8ead954b179abdb0a9
Omar <onha2001@fedal.com>
2018-01-28 18:18:24 +0100

    led added

--- resources/led.py
+++ resources/led.py
@@ -19,19 +19,19 @@ class Led(Resource):
         logger.debug("led init")
 
     def get(self, option):
-
-    	if str(option).lower() not in (self.options):
-    		error_msg = "option " + str(option) + "is not supported"
-    		logger.error(error_msg)
-    		abort(400,error=error_msg)
-    	else:
-    		logger.info("option chosen :%s ", option)
-    		return getattr(self,option)
+        if str(option).lower() not in (self.options):
+            error_msg = "option " + str(option) + "is not supported"
+            logger.error(error_msg)
+            abort(400, error=error_msg)
+        else:
+            logger.info("option chosen :%s ", option)
+            return getattr(self, option)
 
     def on(self):
-		logger.info("light on")
+        logger.info("light on")
         GPIO.output(37, True)
 
-	def off(self):
+    def off(self):
         logger.info("light off")
         GPIO.output(37, False)
+
