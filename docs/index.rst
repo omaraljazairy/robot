20e92515d7fe63c95f6b4ad9326853684043228d
Omar <onha2001@fedal.com>
2018-05-12 19:19:49 +0200

    added the tpm

---
+++ docs/index.rst
@@ -0,0 +1,23 @@
+.. MyRobot documentation master file, created by
+   sphinx-quickstart on Mon Mar 26 20:39:51 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to MyRobot's documentation!
+===================================
+
+.. automodule:: resources.direction
+   :members:
+
+.. automodule:: resources.led
+   :members:
+
+.. automodule:: resources.ultrasone
+   :members:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 from resources.direction import Direction
 from resources.ultrasone import Ultrasone
 from resources.led import Led
+from resource.tpr import TPR
 from config import app_config
 import logging
 import logging.config
@@ -25,6 +26,7 @@ api.add_resource(Movement,'/movement/')
 api.add_resource(Direction,'/directions/<string:direction>/')
 api.add_resource(Led,'/leds/<string:option>/')
 api.add_resource(Ultrasone,'/ultrasone/<string:option>/')
+api.add_resource(TPR,'/tpr/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8009, debug=True, threaded=True)
